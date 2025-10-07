5be0dd496310cca1afcc55e31edb96d68917da85
Jpil15 <jpil15@umd.edu>
2025-10-07 19:33:21 -0400

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -1,5 +1,7 @@
 import RPi.GPIO as GPIO
 from time import sleep
+import math
+
 
 GPIO.setmode(GPIO.BCM)
 
@@ -10,3 +12,20 @@ GPIO.setup(p, GPIO.OUT)
 #GPIO.output(p,1)
 
 pwm = GPIO.PWM(p, 500)
+
+f = 0.2
+
+try: 
+	while True: 
+		t = time.time()
+		brightness = (math.sin(2 * math.pi * f * t))**2
+		duty = brightness * 100
+		pwm.ChangeDutyCycle(duty)
+
+
+except KeyboardInterrupt: 
+	pass
+
+
+pwm.stop()
+GPIO.cleanup()
