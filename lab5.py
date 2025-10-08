e4affa8f46be5d700218d855f1788e99f00200d5
Jpil15 <jpil15@umd.edu>
2025-10-08 11:45:56 -0400

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -26,6 +26,9 @@ GPIO.setup(p8, GPIO.OUT)
 GPIO.setup(p9, GPIO.OUT)
 GPIO.setup(p10, GPIO.OUT)
 
+button = 21
+GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
 pwm = GPIO.PWM(p, 500)
 pwm2 = GPIO.PWM(p2, 500) 
 pwm3 = GPIO.PWM(p3, 500)
@@ -51,33 +54,43 @@ try:
 	pwm9.start(0)
 	pwm10.start(0)
 
+d = 1
+
+def myCallback(button):
+	print("switch direction")
+	d = d * (-1)
+
+
+gpio.add_event_detect(button, gpio.RISING, callback=myCallback, bouncetime = 100)
+
+
 	while True: 
 		t = time.time()
-		brightness = (math.sin(2 * math.pi * f * t))**2
+		brightness = (math.sin(2 * math.pi * f * t * d))**2
 		duty = brightness * 100
 		pwm.ChangeDutyCycle(duty)
 
-		brightness2 = (math.sin((2 * math.pi * f * t) - 0.285))**2
+		brightness2 = (math.sin((2 * math.pi * f * t) - 0.285* d))**2
 		duty2 = brightness2 * 100
 		pwm2.ChangeDutyCycle(duty2)
 
-		brightness3 = (math.sin((2 * math.pi * f * t) - 0.572))**2
+		brightness3 = (math.sin((2 * math.pi * f * t) - 0.572 * d))**2
 		duty3 = brightness3 * 100
 		pwm3.ChangeDutyCycle(duty3)
 
-		brightness4 = (math.sin((2 * math.pi * f * t) - 0.857))**2
+		brightness4 = (math.sin((2 * math.pi * f * t) - 0.857 * d))**2
 		duty4 = brightness4 * 100
 		pwm4.ChangeDutyCycle(duty4)
 
-		brightness5 = (math.sin((2 * math.pi * f * t) - 1.142))**2
+		brightness5 = (math.sin((2 * math.pi * f * t) - 1.142 * d))**2
 		duty5 = brightness5 * 100
 		pwm5.ChangeDutyCycle(duty5)
 
-		brightness6 = (math.sin((2 * math.pi * f * t) - 1.428))**2
+		brightness6 = (math.sin((2 * math.pi * f * t) - 1.428 * d))**2
 		duty6 = brightness6 * 100
 		pwm6.ChangeDutyCycle(duty6)
 
-		brightness7 = (math.sin((2 * math.pi * f * t) - 1.713))**2
+		brightness7 = (math.sin((2 * math.pi * f * t) - 1.713 * d))**2
 		duty7 = brightness7 * 100
 		pwm7.ChangeDutyCycle(duty7)
 
@@ -85,13 +98,16 @@ try:
 		duty8 = brightness8 * 100
 		pwm8.ChangeDutyCycle(duty8)
 
-		brightness9 = (math.sin((2 * math.pi * f * t) - 2.285))**2
+		brightness9 = (math.sin((2 * math.pi * f * t) - 2.285 * d))**2
 		duty9 = brightness9 * 100
 		pwm9.ChangeDutyCycle(duty9)
 
-		brightness10 = (math.sin((2 * math.pi * f * t) - 2.57))**2
+		brightness10 = (math.sin((2 * math.pi * f * t) - 2.57 * d))**2
 		duty10 = brightness10 * 100
 		pwm10.ChangeDutyCycle(duty10)
+
+
+
 
 
 
