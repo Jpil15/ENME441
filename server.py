fc44db21df9225e02065314fcc32844e35c286c8
Jpil15 <jpil15@umd.edu>
2025-10-28 21:05:49 -0400

    Update server.py

--- server.py
+++ server.py
@@ -13,8 +13,24 @@ while True:
     request = conn.recv(1024).decode()
     print("Request:", request)
 
+
+    html = """\
+    <html>
+        <body>
+            <h2>LED Control</h2>
+            <form action="/" method="POST">
+                <input type="radio" name="led" value="1"> LED 1<br>
+                <input type="radio" name="led" value="2"> LED 2<br>
+                <input type="radio" name="led" value="3"> LED 3<br>
+                <input type="range" name="brightness" min="0" max="100" value="50"><br>
+                <input type="submit" value="Set Brightness">
+            </form>
+        </body>
+    </html>"""
+    
+
     # Basic HTTP response
     response = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
-    response += "<html><body><form action="/" method="POST"><input type="radio" name="led" value="1"> LED 1<br><input type="radio" name="led" value="2"> LED 2<br><input type="radio" name="led" value="3"> LED 3<br><input type="range" name="brightness" min="0" max="100" value="50"><br><input type="submit" value="Set Brightness"></form></body></html>"
+    response += html
     conn.sendall(response.encode())
     conn.close()
