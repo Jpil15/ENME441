cec456f51e0ea748f7c00e09caf1928ec0c84789
Jpil15 <jpil15@umd.edu>
2025-11-12 09:59:40 -0500

    Update stepper_class_shiftregister_multiprocessing.py

--- stepper_class_shiftregister_multiprocessing.py
+++ stepper_class_shiftregister_multiprocessing.py
@@ -57,12 +57,8 @@ class Stepper:
     def __step(self, dir):
         self.step_state += dir    # increment/decrement the step
         self.step_state %= 8      # ensure result stays in [0,7]
-        mask = 0b1111 << self.shifter_bit_start
-        with self.lock:
-            # clear this motor's 4 bits, then OR in the new pattern
-            Stepper.shifter_outputs = (Stepper.shifter_outputs & ~mask) | \
-                              ((Stepper.seq[self.step_state] << self.shifter_bit_start) & mask)
-            self.s.shiftByte(Stepper.shifter_outputs)
+        Stepper.shifter_outputs |= 0b1111<<self.shifter_bit_start
+        Stepper.shifter_outputs &= Stepper.seq[self.step_state]<<self.shifter_bit_start
         self.s.shiftByte(Stepper.shifter_outputs)
         self.angle += dir/Stepper.steps_per_degree
         self.angle %= 360         # limit to [0,359.9+] range
